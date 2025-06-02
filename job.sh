b548a0af8255cec5708d279e252b6b7ddd0ff997
Riccardo Infascelli <151134287+RiccardoInfascelli01@users.noreply.github.com>
2025-06-02 13:55:37 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -6,10 +6,10 @@
 #SBATCH --partition=g100_usr_dbg
 #SBATCH --ntasks=1
 
-# 1. Carica Singularity
 module load singularity
 
-# Creo la cartella input se non esiste
+# 1. Pulisci la cartella random_images
+rm -rf random_images
 mkdir -p random_images
 
 # 2. Genera immagini casuali
@@ -18,9 +18,9 @@ for i in $(seq 0 $((NUM_IMAGES - 1))); do
     python3 random_gen_images.py "$i"
 done
 
-# 3. Crea cartella di output
+# 3. Crea cartella di output per immagini convertite
 OUTPUT_DIR="grayscale_output"
 mkdir -p "$OUTPUT_DIR"
 
-# 4. Esegui il programma (input: random_images/, output: grayscale_output/)
+# 4. Esegui il programma di conversione sulle immagini generate
 singularity run image_converter.sif random_images "$OUTPUT_DIR" Average
